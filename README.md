5d584c3362b17de5f856e680ee452f8a13e383e5
whacked-xps <whacked@users.noreply.github.com>
2023-03-03 16:53:40 +0800

    update readme, nix shell env with initializer logic

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # jupyter roadtrip
 
-an opinionated, batteries-included jupyter environment to faciliate notebook usage, maintenance, and interoperation
+an opinionated, `nbdev`-enabled, batteries-included jupyter environment to faciliate notebook usage, maintenance, and interoperation
+
+## latest "works for me" version
+
+these versions should correspond to the hand-locked versions specified in the shell.nix environment
+
+```
+$ jupyter --version
+Selected Jupyter core packages...
+IPython          : 8.11.0
+ipykernel        : 6.21.2
+ipywidgets       : not installed
+jupyter_client   : 8.0.3
+jupyter_core     : 5.2.0
+jupyter_server   : 2.0.0
+jupyterlab       : not installed
+nbclient         : 0.7.2
+nbconvert        : 7.2.9
+nbformat         : 5.7.3
+notebook         : 6.2.0
+qtconsole        : not installed
+traitlets        : 5.9.0
+```
 
 # prequisites
 
@@ -8,5 +30,26 @@ a working [Nix package manager environment](https://nixos.org/download.html)
 
 # usage
 
-nix-shell
+enter the environment with `nix-shell`.
+
+once in the shell, you should be able to run `jupyter notebook` and any of the `nbdev_*` functions
+
+# details
+
+This environment installs all packages necessary to run a working [nbdev](https://nbdev.fast.ai/) notebook environment, including the (2 recommended extensions)[https://nbdev.fast.ai/tutorials/tutorial.html#install-collapsible-headings-and-toc2] (Collapsible headings and TOC2) listed in the [tutorial](https://nbdev.fast.ai/tutorials/tutorial.html).
+
+In total, this install enables these useful extensions:
+
+- nbextensions manager tab inthe jupyter main view
+- Collapsible Headings: collapse markdown sections
+- Codefolding: fold code in cells
+- ExecuteTime: shows last executed time + execution duration
+- toggle line numbers in cells
+- Freeze: mark cells read-only / frozen
+- Scratchpad: throw-away / temporary code execution area
+- Autopep8: auto-format / clean up cell code using pep8
+- datestamper: install current datestamp
+- Table of Contents (2): shows navigation panel to jump to markdown section headings
+- Variable Inspector
+- LC multi outputs (whacked's fork): keeps repeated execution of the same cell into separate, timestamped tabs
 
--- shell.nix
+++ shell.nix
@@ -11,7 +11,7 @@ in helpers.mkShell [
     pkgs.gnumake
 
     pkgs.python310Full
-    pkgs.python310Packages.jupyter
+    pkgs.python310Packages.autopep8
     pkgs.python310Packages.toolz
     pkgs.python310Packages.matplotlib
     pkgs.python310Packages.numpy
@@ -28,17 +28,54 @@ in helpers.mkShell [
   nativeBuildInputs = [
     (builtins.fetchurl {
       url = "https://raw.githubusercontent.com/whacked/setup/master/bash/nix_shortcuts.sh";
-      sha256 = "04qfbcs9ar1pp616bqkpi2vc5jhjxlgk53n26akal97hfriz95rl";
+      sha256 = "0pj6ml8hzxn6hzmxhghnq09cs7s5ibjq7hd649j198la7jx4vin8";
     })
   ];
 
   shellHook = ''
+    ### SETUP
     export VIRTUAL_ENV=''${VIRTUAL_ENV-$PWD/venv}
-    setup-venv() {
+    # FIX for ImportError: libstdc++.so.6: cannot open shared object file: No such file or directory
+    # but note that gcc may be a costly import
+    export LD_LIBRARY_PATH=${pkgs.gcc-unwrapped.lib}/lib:$LD_LIBRARY_PATH
+    setup-venv() {  # install all expected virutalenv packages
+      # see note at https://stackoverflow.com/a/65599505
+      # on nb extensions (in)compatibility.
+      # below versions are from trial and error
       pip install \
-        nbdev==2.3.12
+        notebook==6.2.0 \
+        jupyter_server==2.0.0 \
+        jupyter_core==5.2.0 \
+        nbconvert==7.2.9 \
+        nbformat==5.7.3 \
+        nbdev==2.3.12 \
+        jupyter_contrib_nbextensions==0.5.1 \
+        jupyter_nbextensions_configurator
+      jupyter contrib nbextension install --user
+      ENABLE_BUNDLED_EXTENSIONS=(
+          code_prettify/autopep8
+          codefolding/main
+          collapsible_headings/main
+          contrib_nbextensions_help_item/main
+          datestamper/main
+          execute_time/ExecuteTime
+          freeze/main
+          scratchpad/main
+          toc2/main
+          toggle_all_line_numbers/main
+          varInspector/main
+      );
+      for ext in ''${ENABLE_BUNDLED_EXTENSIONS[@]}; do
+          jupyter nbextension enable $ext
+      done
+      pip install git+https://github.com/whacked/Jupyter-multi_outputs
+      jupyter nbextension install --py lc_multi_outputs --user
+      jupyter nbextension enable --py lc_multi_outputs --user
     }
     ensure-venv setup-venv
+  '' + ''
+    ### SHORTCUTS
+    alias start-notebook="jupyter notebook"
   '' + ''
     echo-shortcuts ${__curPos.file}
   '';  # join strings with +
